fc04ccdaa088e27867da217044c2b5deef1f28cd
dmrgn <danielrmorgan11@gmail.com>
2023-11-23 19:50:04 -0500

    scripts & cleanup

---
+++ backend/scripts/restart.sh
@@ -0,0 +1,2 @@
+# restarts the service responsible for hosting the nginx server
+sudo service helloworld restart
---
+++ backend/scripts/update.sh
@@ -0,0 +1,5 @@
+# pulls the latest version of the server from github
+# and restarts the server
+
+sudo git pull
+sudo sh ./restart.sh
