289fbd1cb147073f1e6c5fd2a4cc43ed14a11fb8
dmrgn <danielrmorgan11@gmail.com>
2023-11-23 20:01:28 -0500

    admin commands

---
+++ backend/scripts/remove_admin.py
@@ -0,0 +1,26 @@
+import sqlite3
+import sys
+con = sqlite3.connect("../data/user.sqlite", check_same_thread=False)
+cur = con.cursor()
+
+res = cur.execute("SELECT * FROM user WHERE email = (?)", (sys.argv[1],))
+print("Num users found:",res.arraysize)
+
+if res.arraysize != 1:
+    print("Error:\n\tInvalid number of users found.")
+    quit()
+
+user = res.fetchone()
+
+print("User name:", user[0])
+print("Email:", user[1])
+print("Tokens:", user[3])
+
+if user[3] != -1:
+    print("Error:\n\tUser is not an admin.")
+    quit()
+
+cur.execute("UPDATE user SET tokens = (?) WHERE email = (?)", (10, user[1]))
+con.commit()
+
+print("User is no longer an admin.")
