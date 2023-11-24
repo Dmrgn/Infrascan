f6cb64168fefd6771cbfebfbe6e51322bd5dfc86
dmrgn <danielrmorgan11@gmail.com>
2023-11-23 20:03:58 -0500

    scripts

--- backend/scripts/remove_admin.py
+++ backend/scripts/remove_admin.py
@@ -4,9 +4,9 @@ con = sqlite3.connect("../data/user.sqlite", check_same_thread=False)
 cur = con.cursor()
 
 res = cur.execute("SELECT * FROM user WHERE email = (?)", (sys.argv[1],))
-print("Num users found:",res.arraysize)
+print("Num users found:",res.rowcount)
 
-if res.arraysize != 1:
+if res.rowcount != 1:
     print("Error:\n\tInvalid number of users found.")
     quit()
 
